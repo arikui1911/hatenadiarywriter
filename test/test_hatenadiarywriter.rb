6dcf98e6063c797f8454ab28dcf4d66f3c8a3c06
Arikui <arikui.ruby@gmail.com>
2015-02-20 23:41:06 +0900

    add test for config

--- test/test_hatenadiarywriter.rb
+++ test/test_hatenadiarywriter.rb
@@ -1,8 +1,17 @@
-require 'test/unit'
-require 'flexmock/test_unit'
+require 'test-unit'
+require 'flexmock'
 require 'hatenadiarywriter'
 require 'hatenadiarywriter/version'
 require 'tmpdir'
+
+module WithTestFilepath
+  def with_test_filepath
+    file = "#{Time.now.to_i}.txt"
+    Dir.mktmpdir {|dir|
+      yield File.join(dir, file)
+    }
+  end
+end
 
 class TestHatenaDiaryWriter < Test::Unit::TestCase
   def test_version
@@ -10,88 +19,219 @@ class TestHatenaDiaryWriter < Test::Unit::TestCase
   end
 end
 
+class TestHatenaDiaryWriterConfig < Test::Unit::TestCase
+  include WithTestFilepath
+
+  setup do
+    @config = HatenaDiaryWriter::Config.new
+  end
+
+  data do
+    set = {
+      username:        nil,
+      groupname:       nil,
+      password:        nil,
+      cookie_file:     nil,
+      http_proxy:      nil,
+      client_encoding: Encoding::UTF_8,
+      server_encoding: Encoding::UTF_8,
+      filter_command:  nil,
+      diary_dir:       ".",
+      diary_glob:      "*.txt",
+      touch_file:      "touch.txt",
+    }
+    set.each{|k, v| set[k] = [k, v] }
+    set
+  end
+  test "initial_values" do |data|
+    item, expected = data
+    assert_equal expected, @config[item]
+  end
+
+  sub_test_case "#load" do
+    data do
+      set = {
+        username:        100,
+        groupname:       200,
+        password:        300,
+        cookie_file:     400,
+        http_proxy:      500,
+        client_encoding: 600,
+        server_encoding: 700,
+        filter_command:  800,
+        diary_dir:       900,
+        diary_glob:      123,
+        touch_file:      456,
+      }
+      set.each{|k, v| set[k] = [k, v] }
+      set
+    end
+    test "load values" do |data|
+      src = <<-EOS
+username:        100
+groupname:       200
+password:        300
+cookie_file:     400
+http_proxy:      500
+client_encoding: 600
+server_encoding: 700
+filter_command:  800
+diary_dir:       900
+diary_glob:      123
+touch_file:      456
+      EOS
+
+      with_test_filepath {|path|
+        File.write path, src
+        @config.load(path)
+      }
+      item, expected = data
+      assert_equal expected, @config[item]
+    end
+
+    data(id:     [:username, 100],
+         g:      [:groupname, 200],
+         cookie: [:cookie_file, 300],
+         proxy:  [:http_proxy, 400],
+         filter: [:filter_command, 500],
+         touch:  [:touch_file, 600])
+    test "aliased names" do |data|
+      src = <<-EOS
+id:     100
+g:      200
+cookie: 300
+proxy:  400
+filter: 500
+touch:  600
+      EOS
+
+      with_test_filepath {|path|
+        File.write path, src
+        @config.load(path)
+      }
+      item, expected = data
+      assert_equal expected, @config[item]
+    end
+
+    test "handle invalid item by block" do
+      src = <<-EOS
+id:        user
+groupname: group
+hoge:      HOGEEEEEEEEE
+cookie:    cookie.txt
+proxy:     http://example.com:80
+piyo:      666
+    EOS
+      expected = %w[hoge piyo]
+
+      actual = []
+      with_test_filepath {|path|
+        File.write path, src
+        @config.load(path){|invalid| actual << invalid }
+      }
+      assert_equal expected, actual
+    end
+
+    test "invalid item" do
+      src = <<-EOS
+id:        user
+groupname: group
+hoge:      HOGEEEEEEEEE
+    EOS
+
+      with_test_filepath {|path|
+        File.write path, src
+        e = assert_raise(NameError){ @config.load(path) }
+        assert_equal 'hoge', e.name
+      }
+    end
+  end
+end
+
 class TestHatenaDiaryWriterUtils < Test::Unit::TestCase
-  def setup
+  include FlexMock::TestCase
+  include WithTestFilepath
+
+  setup do
     @m = HatenaDiaryWriter::Utils
   end
 
-  def test_read_input
-    flexmock(:safe, $stdin){|mi|
-      mi.should_receive(:tty?).and_return(true)
-      mi.should_receive(:gets).and_return("readed text.\n")
-      flexmock(:safe, $stderr){|me|
-        me.should_receive(:print).with("prompt: ")
-        assert_equal "readed text.", @m.read_input("prompt: ")
+  sub_test_case "read_input" do
+    test "ordinary case" do
+      flexmock(:safe, $stdin){|mi|
+        mi.should_receive(:tty?).and_return(true)
+        mi.should_receive(:gets).and_return("readed text.\n")
+        flexmock(:safe, $stderr){|me|
+          me.should_receive(:print).with("prompt: ")
+          assert_equal "readed text.", @m.read_input("prompt: ")
+        }
       }
-    }
-  end
+    end
 
-  def test_read_input_no_nl_in_input
-    flexmock(:safe, $stdin){|mi|
-      mi.should_receive(:tty?).and_return(true)
-      mi.should_receive(:gets).and_return("readed text.")
-      flexmock(:safe, $stderr){|me|
-        me.should_receive(:print).with("prompt: ")
-        me.should_receive(:puts).with()
-        assert_equal "readed text.", @m.read_input("prompt: ")
+    test "no newline in input" do
+      flexmock(:safe, $stdin){|mi|
+        mi.should_receive(:tty?).and_return(true)
+        mi.should_receive(:gets).and_return("readed text.")
+        flexmock(:safe, $stderr){|me|
+          me.should_receive(:print).with("prompt: ")
+          me.should_receive(:puts).with()
+          assert_equal "readed text.", @m.read_input("prompt: ")
+        }
       }
-    }
-  end
+    end
 
-  def test_read_input_noecho
-    flexmock(:safe, $stdin){|mi|
-      mi.should_receive(:tty?).and_return(true)
-      mi.should_receive(:noecho).and_yield($stdin)
-      mi.should_receive(:gets).and_return("readed text.\n")
-      flexmock(:safe, $stderr){|me|
-        me.should_receive(:print).with("prompt: ")
-        me.should_receive(:puts).with()
-        assert_equal "readed text.", @m.read_input("prompt: ", false)
+    test "noecho" do
+      flexmock(:safe, $stdin){|mi|
+        mi.should_receive(:tty?).and_return(true)
+        mi.should_receive(:noecho).and_yield($stdin)
+        mi.should_receive(:gets).and_return("readed text.\n")
+        flexmock(:safe, $stderr){|me|
+          me.should_receive(:print).with("prompt: ")
+          me.should_receive(:puts).with()
+          assert_equal "readed text.", @m.read_input("prompt: ", false)
+        }
       }
-    }
-  end
+    end
 
-  def test_read_input_no_tty
-    flexmock(:safe, $stdin){|mock|
-      mock.should_receive(:tty?).and_return(false)
-      assert_equal nil, @m.read_input("prompt: ")
-    }
-  end
-
-  def test_parse_proxy_url
-    assert_equal ["http://example.com", 8080], @m.parse_proxy_url("http://example.com:8080")
-  end
-
-  def test_parse_proxy_url_without_scheme
-    assert_equal ["http://example.com", 8080], @m.parse_proxy_url("example.com:8080")
-  end
-
-  def test_parse_proxy_url_only_host
-    assert_equal ["http://example.com", 80], @m.parse_proxy_url("example.com")
-  end
-
-  def test_parse_proxy_url_invalid_scheme
-    assert_raise ArgumentError do
-      @m.parse_proxy_url("ftp://example.com:1234")
+    test "no tty" do
+      flexmock(:safe, $stdin){|mock|
+        mock.should_receive(:tty?).and_return(false)
+        assert_equal nil, @m.read_input("prompt: ")
+      }
     end
   end
 
-  def test_guess_similar_one
-    assert_equal "Ruby", @m.guess_similar_one("ruby", %w[Perl Python Ruby Java Lisp])
+  sub_test_case "parse_proxy_url()" do
+     test "parse host and port" do
+      assert_equal ["http://example.com", 8080], @m.parse_proxy_url("http://example.com:8080")
+    end
+
+    test "without scheme" do
+      assert_equal ["http://example.com", 8080], @m.parse_proxy_url("example.com:8080")
+    end
+
+    test "only host" do
+      assert_equal ["http://example.com", 80], @m.parse_proxy_url("example.com")
+    end
+
+    test "invalid scheme" do
+      assert_raise ArgumentError do
+        @m.parse_proxy_url("ftp://example.com:1234")
+      end
+    end
   end
 
-  def test_guess_similar_one_too_short
-    assert_nil @m.guess_similar_one("C", %w[A B D R Q])
+  sub_test_case "guess_similar_one()" do
+    test "get correct one" do
+      assert_equal "Ruby", @m.guess_similar_one("Tuby", %w[Perl Python Ruby Java Lisp])
+    end
+
+    test "too short" do
+      assert_nil @m.guess_similar_one("C", %w[A B D R Q])
+    end
   end
 
-  def with_test_filepath
-    file = "#{Time.now.to_i}.txt"
-    Dir.mktmpdir {|dir|
-      yield File.join(dir, file)
-    }
-  end
-
-  def test_change_file_content
+  test "change_file_content" do
     src = <<-EOS
 Ruby
 Perl
@@ -116,7 +256,7 @@ Python
     }
   end
 
-  def test_open_with_command_filter
+  test "open_with_command_filter" do
     src = <<-EOS
 M4A1
 M1911A1
